799831f92b5b289854c4b81a103578109d769803
Shota Kashihara <mercury4th@gmail.com>
2021-01-15 19:22:12 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 echo $SCRIPT_DIR
 
+# ./config/ 以下の各ファイルのシンボリックリンクを ~/ 以下に貼る
 for FILE in `find config -type f | sed "s/config\///"`; do 
   ln -snfv ${SCRIPT_DIR}/config/${FILE} ~/${FILE}
 done
