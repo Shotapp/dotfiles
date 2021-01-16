e8d6c7500c6133b35d21b5bd784e190b053ca12c
Shota Kashihara <mercury4th@gmail.com>
2021-01-17 02:33:43 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 echo $SCRIPT_DIR
 
 # ./config/ 以下の各ファイルのシンボリックリンクを ~/ 以下に貼る
+IFS=$'\n' # for-in delimiter を改行のみにする
 for FILE in `find config -type f | sed "s/config\///"`; do 
   mkdir -p ~/$(dirname ${FILE})
   ln -snfv ${SCRIPT_DIR}/config/${FILE} ~/${FILE}
 done
+IFS=$' \t\n'
 
 source ~/.zshrc
