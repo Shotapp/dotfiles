ff9937a275835bb1d64c2f4940dc7e5de1e3675f
Shota Kashihara <mercury4th@gmail.com>
2021-07-16 14:49:29 +0900

    update

--- config/.zsh/exports.zsh
+++ config/.zsh/exports.zsh
@@ -30,7 +30,6 @@ if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 # pyenv
 export PYENV_ROOT=$HOME/.pyenv
 export PATH=$PYENV_ROOT/bin:$PATH
-eval "$(pyenv init --path)"
 
 # nodebrew
 export PATH=$HOME/.nodebrew/current/bin:$PATH
